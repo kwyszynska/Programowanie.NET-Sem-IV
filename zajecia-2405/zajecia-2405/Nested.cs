19808b72da36e8f85b19478d8aaed2862aa22e44
Katarzyna Wyszyńska <74304637+kwyszynska@users.noreply.github.com>
2022-06-15 14:32:01 +0200

    Add files via upload

---
+++ zajecia-2405/zajecia-2405/Nested.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zajecia_2405
+{
+    internal class Nested
+    {
+        public class Calculate
+        {
+            public void squareRoot(float number)
+            {
+                float y = 0.0000001f;
+
+                float result = number / 2;
+
+                while (Math.Abs(result - (number / result)) > y)
+                {
+                    result = (result + (number / result)) / 2;
+
+                    if (result * result == 2)
+                    {
+                        Console.WriteLine(result);
+                    }
+                }
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
---
+++ zajecia-2405/zajecia-2405/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zajecia_2405
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Nested.Calculate Calculate = new Nested.Calculate();
+            Calculate.squareRoot(3); 
+            Calculate.squareRoot(5); 
+            Calculate.squareRoot(37);
+            Console.ReadKey();
+        }
+    }
+}
